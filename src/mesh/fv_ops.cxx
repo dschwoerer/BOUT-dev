07b2602ad911dbd13f40e42b23044ed52f231e09
dschwoerer <dschwoerer@users.noreply.github.com>
2022-11-17 12:07:31 +0000

    Apply clang-format changes

--- src/mesh/fv_ops.cxx
+++ src/mesh/fv_ops.cxx
@@ -8,57 +8,57 @@
 
 namespace FV {
 
-  // Div ( a Grad_perp(f) ) -- ∇⊥ ( a ⋅ ∇⊥ f) --  Vorticity
-  Field3D Div_a_Grad_perp(const Field3D& a, const Field3D& f) {
-    ASSERT2(a.getLocation() == f.getLocation());
+// Div ( a Grad_perp(f) ) -- ∇⊥ ( a ⋅ ∇⊥ f) --  Vorticity
+Field3D Div_a_Grad_perp(const Field3D& a, const Field3D& f) {
+  ASSERT2(a.getLocation() == f.getLocation());
 
-    Mesh* mesh = a.getMesh();
+  Mesh* mesh = a.getMesh();
 
-    Field3D result{zeroFrom(f)};
+  Field3D result{zeroFrom(f)};
 
-    Coordinates* coord = f.getCoordinates();
+  Coordinates* coord = f.getCoordinates();
 
-    // Flux in x
+  // Flux in x
 
-    int xs = mesh->xstart - 1;
-    int xe = mesh->xend;
+  int xs = mesh->xstart - 1;
+  int xe = mesh->xend;
 
-    for (int i = xs; i <= xe; i++) {
-      for (int j = mesh->ystart; j <= mesh->yend; j++) {
-	for (int k = 0; k < mesh->LocalNz; k++) {
-	  // Calculate flux from i to i+1
+  for (int i = xs; i <= xe; i++) {
+    for (int j = mesh->ystart; j <= mesh->yend; j++) {
+      for (int k = 0; k < mesh->LocalNz; k++) {
+        // Calculate flux from i to i+1
 
-	  BoutReal fout = 0.5 * (a(i, j, k) + a(i + 1, j, k))
-                          * (coord->J(i, j, k) * coord->g11(i, j, k)
-                             + coord->J(i + 1, j, k) * coord->g11(i + 1, j, k))
-                          * (f(i + 1, j, k) - f(i, j, k))
-                          / (coord->dx(i, j, k) + coord->dx(i + 1, j, k));
+        BoutReal fout = 0.5 * (a(i, j, k) + a(i + 1, j, k))
+                        * (coord->J(i, j, k) * coord->g11(i, j, k)
+                           + coord->J(i + 1, j, k) * coord->g11(i + 1, j, k))
+                        * (f(i + 1, j, k) - f(i, j, k))
+                        / (coord->dx(i, j, k) + coord->dx(i + 1, j, k));
 
-	  result(i, j, k) += fout / (coord->dx(i, j, k) * coord->J(i, j, k));
-	  result(i + 1, j, k) -= fout / (coord->dx(i + 1, j, k) * coord->J(i + 1, j, k));
-	}
+        result(i, j, k) += fout / (coord->dx(i, j, k) * coord->J(i, j, k));
+        result(i + 1, j, k) -= fout / (coord->dx(i + 1, j, k) * coord->J(i + 1, j, k));
       }
     }
+  }
 
-    const bool fci = f.hasParallelSlices() && a.hasParallelSlices();
+  const bool fci = f.hasParallelSlices() && a.hasParallelSlices();
 
-    if (bout::build::use_metric_3d and fci) {
-      // 3D Metric, need yup/ydown fields.
-      // Requires previous communication of metrics
-      // -- should insert communication here?
-      if (!coord->g23.hasParallelSlices() || !coord->g_23.hasParallelSlices()
-          || !coord->dy.hasParallelSlices() || !coord->dz.hasParallelSlices()
-          || !coord->Bxy.hasParallelSlices() || !coord->J.hasParallelSlices()) {
-        throw BoutException("metrics have no yup/down: Maybe communicate in init?");
-      }
+  if (bout::build::use_metric_3d and fci) {
+    // 3D Metric, need yup/ydown fields.
+    // Requires previous communication of metrics
+    // -- should insert communication here?
+    if (!coord->g23.hasParallelSlices() || !coord->g_23.hasParallelSlices()
+        || !coord->dy.hasParallelSlices() || !coord->dz.hasParallelSlices()
+        || !coord->Bxy.hasParallelSlices() || !coord->J.hasParallelSlices()) {
+      throw BoutException("metrics have no yup/down: Maybe communicate in init?");
     }
+  }
 
-    // Y and Z fluxes require Y derivatives
+  // Y and Z fluxes require Y derivatives
 
-    // Fields containing values along the magnetic field
+  // Fields containing values along the magnetic field
 
-    // Values on this y slice (centre).
-    // This is needed because toFieldAligned may modify the field
+  // Values on this y slice (centre).
+  // This is needed because toFieldAligned may modify the field
 #define GET(fci, a, b)				    \
   const Field3D a##c = fci ? b : toFieldAligned(b); \
   const Field3D a##up = fci ? b.yup() : a##c;       \
@@ -149,7 +149,7 @@ namespace FV {
     }
     
     return result;
-  }
+}
 
   const Field3D Div_par_K_Grad_par(const Field3D &Kin, const Field3D &fin, bool bndry_flux) {
     TRACE("FV::Div_par_K_Grad_par");
