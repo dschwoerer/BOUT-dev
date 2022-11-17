1e5a8850da95e9bcbdaeca615185de08be1d0c02
David Bold <dave@ipp.mpg.de>
2022-11-17 13:37:10 +0100

    CI: Add testing with new PETSc

---
+++ .build_petsc_for_ci.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+if test $BUILD_PETSC ; then
+    if [[ ! -d $HOME/local/include/petsc ]]; then
+	echo "****************************************"
+	echo "Building PETSc"
+	echo "****************************************"
+
+	git clone -b release https://gitlab.com/petsc/petsc.git petsc --depth=1
+
+	./configure \
+	    --with-mpi=yes \
+	    --with-precision=double \
+	    --with-scalar-type=real \
+	    --with-shared-libraries=1 \
+	    --with-debugging=0 \
+	    {C,CXX,F}OPTFLAGS="-O3 -march=native" \
+	    --prefix=$HOME/local/petsc
+
+	make && make install
+
+	echo "****************************************"
+	echo " Finished building PETSc"
+	echo "****************************************"
+    else
+	echo "****************************************"
+	echo " PETSc already installed"
+	echo "****************************************"
+    fi
+else
+    echo "****************************************"
+    echo " PETSc not requested"
+    echo "****************************************"
+fi
