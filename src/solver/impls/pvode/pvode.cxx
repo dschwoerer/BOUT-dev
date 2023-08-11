424482d4a63f7ce55730555a2e3d9b654ec28fdd
David Bold <dave@ipp.mpg.de>
2023-08-11 17:02:44 +0200

    set var name in pvode

--- src/solver/impls/pvode/pvode.cxx
+++ src/solver/impls/pvode/pvode.cxx
@@ -371,6 +371,7 @@ BoutReal PvodeSolver::run(BoutReal tout) {
 
       for (auto& f : f3d) {
         f.F_var->enableTracking(fmt::format("ddt_{:s}", f.name), debug);
+	setName(f.var, f.name);
       }
       run_rhs(simtime);
 
